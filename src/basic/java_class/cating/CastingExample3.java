e4ac9122e05699c55cae26846a2492d3e2e17a23
kimtaekjin <qlcdmlrlf@naver.com>
2024-02-20 17:17:11 +0900

    #initial commit 형변환

---
+++ src/basic/java_class/cating/CastingExample3.java
@@ -0,0 +1,21 @@
+package basic.java_class.cating;
+
+public class CastingExample3 {
+    public static void main(String[] args) {
+
+
+        char c='B';
+        int i=2;
+        int intResult = c + i;
+        char charResult = (char) (c+i);
+        System.out.println(intResult);
+        System.out.println(charResult);
+        
+        
+        int k=10;
+        double d= k /4.0;
+        System.out.println("d = " + d);
+
+
+    }
+}
