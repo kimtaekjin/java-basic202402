8a6b447f988cccca0d4c380af24abd75b6b4e612
kimtaekjin <qlcdmlrlf@naver.com>
2024-02-20 17:16:32 +0900

    #initial commit

---
+++ src/basic/java_class/BooleanExample.java
@@ -0,0 +1,18 @@
+package basic.java_class;
+
+public class BooleanExample {
+
+    public static void main(String[] args) {
+
+
+        boolean b1=true;
+        boolean b2=false;
+
+//        boolean b3=True;
+//        boolean b4=False;
+//        boolean b5=0;
+//        boolean b6="논리형 타입";
+        //(x) 허용하지 않는다.
+
+    }
+}
