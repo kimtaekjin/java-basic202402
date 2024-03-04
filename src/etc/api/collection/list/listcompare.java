23db77579aae4b392aeb53e5bfdd4da57d3aa5cc
kimtaekjin <qlcdmlrlf@naver.com>
2024-03-04 17:54:37 +0900

    #java  oop(list)

--- src/etc/api/collection/list/listcompare.java
+++ src/etc/api/collection/list/listcompare.java
@@ -2,6 +2,7 @@ package etc.api.collection.list;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class listcompare {
@@ -15,19 +16,21 @@ public class listcompare {
 
 
         List<Integer> array=new ArrayList<>();
-        List<Integer> linked=new ArrayList<>();
+        List<Integer> linked=new LinkedList<>();
 
         for (int i = 0; i <1000000 ; i++) {
             array.add(i);
             linked.add(i);
         }
 
+
         long start,end;
 
         //배열리스트에서 데이터 제어
         start = System.currentTimeMillis();
         for (int i = 0; i < array.size(); i++) {
-            array.remove(0);
+//            array.remove(0);
+            array.get(i);
         }
         end=System.currentTimeMillis();
 
@@ -38,8 +41,11 @@ public class listcompare {
 
         start = System.currentTimeMillis();
         for (int i = 0; i < array.size(); i++) {
-            linked.remove(0);
+//            linked.remove(0);
+            linked.get(i);
+
         }
+
         end=System.currentTimeMillis();
 
 
